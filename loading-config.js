2dc97db9d274c04829f7c4d0e6dcd57599b3e015
Rob Work PC <rob.bryan@otoy.com>
2026-03-12 19:29:01 +0000

    chore: untrack loading-config.js from git

--- loading-config.js
+++
@@ -1,5 +0,0 @@
-var VITE_PORT = '3456';
-var AVAILABLE_MODELS = ['acestep-v15-base','acestep-v15-merge-base-sft-0.5','acestep-v15-merge-base-turbo-0.5','acestep-v15-merge-sft-turbo-0.3','acestep-v15-merge-sft-turbo-0.4','acestep-v15-merge-sft-turbo-0.5','acestep-v15-sft','acestep-v15-turbo','acestep-v15-turbo-continuous','acestep-v15-turbo-shift1','acestep-v15-turbo-shift3'];
-var AVAILABLE_LM_MODELS = ['acestep-5Hz-lm-0.6B','acestep-5Hz-lm-1.7B','acestep-5Hz-lm-4B'];
-var CURRENT_MODEL = 'acestep-v15-sft';
-var CURRENT_LM_MODEL = 'acestep-5Hz-lm-4B';
